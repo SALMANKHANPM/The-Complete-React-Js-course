83287bb47b45111a4385ae59f45a13cdb9a87552
Aakash Rao <aakash.codes02@gmail.com>
2022-04-30 06:10:42 +0530

    Added search functionality - half way on input field

--- 01-react-basics/src/App.js
+++ 01-react-basics/src/App.js
@@ -1,57 +1,58 @@
 import { Component } from 'react';
-
-import logo from './logo.svg';
 import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>Hello my name is Aakash!</p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      name: { firstName: 'Aakash', lastName: 'Rao' },
-      company: 'Google',
+      monsters: [],
     };
+    console.log('constructor');
+  }
+
+  componentDidMount() {
+    console.log('Componentdidmount');
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json =>
+        this.setState(
+          () => {
+            return { monsters: json };
+          },
+          () => console.log(this.state)
+        )
+      );
   }
 
   render() {
+    console.log('render');
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hii {this.state.name.firstName} {this.state.name.lastName}! I wanted
-            to work at {this.state.company} ⚛️
-          </p>
-          <button
-            onClick={() => {
-              this.setState({
-                name: { firstName: 'Swapna', lastName: 'Kumar Panda' },
-              });
-              console.log(this.state);
-            }}
-          >
-            Change Name
-          </button>
-        </header>
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={e => {
+            const searchString = e.target.value.toLowerCase();
+
+            //  [{name: 'Aakash}, {name: 'Swapna'}]
+            const filteredMonters = this.state.monsters.filter(monster => {
+              return monster.name.toLowerCase().includes(searchString);
+            });
+
+            this.setState(() => {
+              return { monsters: filteredMonters };
+            });
+          }}
+        />
+        {this.state.monsters.map(monster => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
